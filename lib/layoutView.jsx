40f4c07aded2ffdb69549460997b409e56c5aff3
winton <mail@wintoni.us>
2019-01-19 22:04:50 -0500

    Bootstrapping store state

--- lib/layoutView.jsx
+++ lib/layoutView.jsx
@@ -7,9 +7,20 @@ export default function({ beforeAny, view }) {
   )
 }
 
+function update(prop, arg, dot) {
+  const script = document.createElement("script"),
+    state = JSON.stringify(dot.state.store)
+
+  script.appendChild(
+    document.createTextNode(`window.store=${state};`)
+  )
+
+  this.querySelector("head").appendChild(script)
+}
+
 function render(prop, arg, dot) {
   return (
-    <html lang="en">
+    <html lang="en" update={update}>
       <head>
         <meta charset="utf-8" />
         <title>Render View Test</title>
